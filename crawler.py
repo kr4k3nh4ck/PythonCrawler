2d541aad7aa066a88710d6fcc491e512c2a109dd
kr4k3nh4ck <umuttechin@gmail.com>
2018-09-30 07:16:17 -0700

    Clubs are Crawled

--- crawler.py
+++ crawler.py
@@ -13,7 +13,7 @@ country_keys = []
 country_values = []
 
 main = {}
-
+main_league = {}
 
 for element in country_list:
     new_element = element.replace('"', "").split(">")
@@ -25,14 +25,15 @@ for element in country_list:
 country_dict = dict(zip(country_keys, country_values))
 
 """League Names, Logos and URLs"""
-league_name_list = []
-league_logo_list = []
-league_url_list = []
+
 for key in country_dict:
+    league_name_list = []
+    league_logo_list = []
+    league_url_list = []
     try:
         main[str(country_dict[key])] = []
         a = int(key)
-        if a==40:
+        if a==174:
             payload =int(key)
             league_page = requests.post("https://www.transfermarkt.com.tr/wettbewerbe/national/wettbewerbe/" + str(a), headers={'User-Agent': 'Custom'})
             league_page_content = BeautifulSoup(league_page.content, 'html.parser')
@@ -51,18 +52,22 @@ for key in country_dict:
                     league_url_list.append(league_url_pattern)
 
                     #It should be done at the at
-                    main[str(country_dict[key])].append({"Legaue_Name":league_name,"Legaue_Logo":league_logo,"Legaue_URL":league_url_pattern})
+                    main[str(country_dict[key])].append({"League_Name":league_name, "League_Logo":league_logo, "League_URL":league_url_pattern})
 
                     club_name_list = []
                     club_logo_list = []
                     club_url_list = []
 
-                    """URL PATTERM VE ÜLKE ADIYLA DİCT OLUŞTURUP İÇİNDE DÖN!"""
+            league_dict = dict(zip(league_name_list,league_url_list))
+            for league_key in league_dict:
+                try:
 
-                    if league_url_pattern=="https://www.transfermarkt.com.tr/1-bundesliga/startseite/wettbewerb/L1":
-                        club_page = requests.get(league_url_pattern, headers={'User-Agent': 'Custom'})
-                        club_page_content = BeautifulSoup(club_page.content, 'html.parser')
-                        club_table = club_page_content.find("table", attrs={"class":"items"})
+                    club_page = requests.get(league_dict[league_key], headers={'User-Agent': 'Custom'})
+                    club_page_content = BeautifulSoup(club_page.content, 'html.parser')
+                    club_table = club_page_content.find("table", attrs={"class":"items"})
+                    column_len_of_table = len(club_table.find("thead").findAll("th"))
+                    if column_len_of_table == 6 or column_len_of_table == 10:
+                        main_league[league_key] = []
                         club_table_body = club_table.find("tbody")
                         all_clubs_tr = club_table_body.findAll("tr")
                         for club_cols in all_clubs_tr:
@@ -73,9 +78,18 @@ for key in country_dict:
                             club_logo_list.append(club_logo)
                             club_url = club["href"]
                             club_url_pattern = r"https://www.transfermarkt.com.tr" + club_url
+                            main_league[str(league_key)].append({"Club_Name":club_name, "Club_Logo":club_logo, "Club_URL": club_url_pattern})
+                    else:
+                        pass
 
+                except Exception as e:
+                    print("Club_Name"+ club_name, "Club_Logo" +club_logo, "Club_URL"+  club_url_pattern)
+                    print(league_key)
+                    print(e)
 
 
     except Exception as e:
+        print(e)
         pass
 #print(main)
+print(main_league)
